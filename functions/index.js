325fb31b3d19111a110dd88a50dd9cf333dbeaab
Marco <marcocatta@gmail.com>
2022-04-11 19:28:52 +0200

    First commit

---
+++ functions/index.js
@@ -0,0 +1,22 @@
+const functions = require("firebase-functions")
+const admin = require('firebase-admin')
+const { v4: uuidv4 } = require('uuid')
+
+// Init
+admin.initializeApp()
+
+
+// Check File storage creation
+exports.storage = functions.region('europe-west2').storage.object().onFinalize(async (file) => {
+
+    var fileObject = {
+        name : file.name,
+        description: '',
+        createAt: file.timeCreated,
+        storage: file.storage,
+        bucket: file.bucket
+    };
+
+    console.log(file)
+    return admin.firestore().doc(`/audio/${uuidv4()}`).set(fileObject)
+})
